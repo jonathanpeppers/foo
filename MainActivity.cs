dbee518f36fcf06ec6fb86cd3416c0f53cfcc4c9
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-01-04 11:02:01 -0600

    Update text on screen from C#

--- MainActivity.cs
+++ MainActivity.cs
@@ -14,6 +14,8 @@ namespace foo
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            FindViewById<TextView>(Resource.Id.text).Text = "Hello!";
         }
     }
 }
